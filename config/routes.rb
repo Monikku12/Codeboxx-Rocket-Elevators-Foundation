dc0f092db95d01967aac5cc0204fd92ff327f7e3
Monikku12 <monique.paymentboulanger@gmail.com>
2022-07-02 06:07:00 +0900

    all link done, except ToTop

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'commercial', to: 'commercial#commercialpage', as:'commercial'
-  get 'residential', to: 'residential#residentialpage', as:'residential'
-  get 'index', to: 'index#index', as:'index'
+  get 'commercial', to: 'index#commercialpage', as: "commercial"
+  get 'residential', to: 'index#residentialpage', as: "residential"
+  get 'index', to: 'index#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'index#index'
