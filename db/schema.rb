9770fba204a31306bd77cacf54cc8e4d2f071de4
GDemers71 <gabrieldemers71@gmail.com>
2022-07-05 13:25:51 -0400

    ajouter une migration pour la column quote_email du quotes forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_000315) do
+ActiveRecord::Schema.define(version: 2022_07_05_143852) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "type_of_address"
@@ -133,6 +133,7 @@ ActiveRecord::Schema.define(version: 2022_07_05_000315) do
     t.text "final_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "quote_email"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
