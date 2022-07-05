822eca334edb058e0abc9894db592b2e7be13213
Mathieu <mathieubernier27@gmail.com>
2022-07-05 17:33:54 -0400

    adding links between battery and buildings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_212647) do
+ActiveRecord::Schema.define(version: 2022_07_05_213120) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "type_of_address"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2022_07_05_212647) do
     t.bigint "customer_id"
     t.bigint "buildingdetail_id"
     t.bigint "building_detail_id"
+    t.bigint "battery_id"
+    t.index ["battery_id"], name: "index_buildings_on_battery_id"
     t.index ["building_detail_id"], name: "index_buildings_on_building_detail_id"
     t.index ["buildingdetail_id"], name: "index_buildings_on_buildingdetail_id"
     t.index ["customer_id"], name: "index_buildings_on_customer_id"
@@ -160,6 +162,7 @@ ActiveRecord::Schema.define(version: 2022_07_05_212647) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "buildings", "batteries"
   add_foreign_key "buildings", "building_details"
   add_foreign_key "buildings", "customers"
   add_foreign_key "customers", "users"
