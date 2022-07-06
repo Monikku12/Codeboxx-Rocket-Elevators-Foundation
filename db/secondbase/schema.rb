d0da797d1bb6bdee61b453b0fd08dccc8d61a552
Monikku12 <monique.paymentboulanger@gmail.com>
2022-07-07 00:36:35 +0900

    fixed column name in FactQuotes

--- db/secondbase/schema.rb
+++ db/secondbase/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_103225) do
+ActiveRecord::Schema.define(version: 2022_07_06_151903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2022_07_06_103225) do
     t.date "creation_date"
     t.text "company_name"
     t.text "quote_email"
-    t.integer "Nb_Elevator"
+    t.integer "nb_elevator"
   end
 
 end
