cdf18a75da3ab355c588699fb8b845c2ec2c9bbe
Mathieu <mathieubernier27@gmail.com>
2022-07-07 20:11:08 -0400

    Revert "revert to commit"
    This reverts commit 119cf4e19c8d1f3aa74b9bb4a53fd3c2211aca44.

--- db/migrate/20220705213645_add_elevator_to_column.rb
+++ db/migrate/20220705213645_add_elevator_to_column.rb
@@ -1,5 +1,5 @@
 class AddElevatorToColumn < ActiveRecord::Migration[5.2]
   def change
-    add_reference :elevators, :column, foreign_key: true
+    add_reference :columns, :elevator, foreign_key: true
   end
 end
