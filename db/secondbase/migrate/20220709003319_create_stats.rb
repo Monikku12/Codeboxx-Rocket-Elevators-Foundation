29801c8cfa665849071fad9343c3a4b4064e044e
Monikku12 <monique.paymentboulanger@gmail.com>
2022-07-09 09:39:03 +0900

    Revert "migrate error"
    This reverts commit 7083940b783dfa9d1833430f042a70b3b745e9ed.

--- db/secondbase/migrate/20220709003319_create_stats.rb
+++
@@ -1,9 +0,0 @@
-class CreateStats < ActiveRecord::Migration[5.2]
-  def change
-    create_table :stats do |t|
-      t.integer :monthly_contact
-      t.integer :monthly_quotes
-      t.integer :number_elevator
-    end
-  end
-end
