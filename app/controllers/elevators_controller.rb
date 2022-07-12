a863c1a0f8ba4472f8d54799b02c150918fce527
Mathieu <mathieubernier27@gmail.com>
2022-07-12 12:59:13 -0400

    twilio done and sms message ok

--- app/controllers/elevators_controller.rb
+++ app/controllers/elevators_controller.rb
@@ -73,20 +73,4 @@ class ElevatorsController < ApplicationController
       params.require(:elevator).permit(:serial_number, :model, :type, :status, :commissioning_date, :last_inspection_date, :inspection_certificate, :information, :notes)
     end
 
-
-    # To get the changes to send SMS
-    def do_something
-      if @elevator_status_changed = "intervention" then 
-          
-        account_sid = ENV['TWILIO_ACCOUNT_SID']
-        auth_token = ENV['TWILIO_AUTH_TOKEN']
-        @client = Twilio::REST::Client.new(account_sid, auth_token)
-
-        message = @client.messages.create(
-            to: '+14182346159',
-            from: '++15203416848',
-            body: 'We have trouble ! Come over here !'      
-        )
-      end
-    end
   end    
--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -4,21 +4,21 @@ class Elevator < ApplicationRecord
     after_update :twilio
     
     
-    def test
-        puts "------------------"
-        puts "test"
-        puts "------------------"
+    # def test
+    #     puts "------------------"
+    #     puts "test"
+    #     puts "------------------"
 
-        account_sid = ENV['TWILIO_ACCOUNT_SID']
-        auth_token = ENV['TWILIO_AUTH_TOKEN']
-        @client = Twilio::REST::Client.new(account_sid, auth_token)
+    #     account_sid = ENV['TWILIO_ACCOUNT_SID']
+    #     auth_token = ENV['TWILIO_AUTH_TOKEN']
+    #     @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-        message = @client.messages.create(
-            to: '+14182346159',
-            from: '++15203416848',
-            body: 'We have trouble ! Come over here !'      
-        )
-    end
+    #     message = @client.messages.create(
+    #         to: '+14182346159',
+    #         from: '++15203416848',
+    #         body: 'The status from elevator' {elevator_id} 'in building' {elevator_id.building} 'has changed from' {elevator_id} 'to' {new_elevator_id}     
+    #     )
+    # end
 
 
     def twilio
@@ -30,8 +30,8 @@ class Elevator < ApplicationRecord
   
           message = @client.messages.create(
               to: '+14182346159',
-              from: '++15203416848',
-              body: 'We have trouble ! Come over here !'      
+              from: '+15203416848',
+              body: "The status from elevator #{id} in building #{column.battery.building.id} has changed from #{status_before_last_save} to #{status}."
           )
         end
     end
