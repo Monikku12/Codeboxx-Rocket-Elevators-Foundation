baef12516c87dcbd89a80c0d3cd4cd0da6905439
Monikku12 <monique.paymentboulanger@gmail.com>
2022-09-08 22:46:15 +0900

    modified deploy.rb with set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :rbenv_roles, :all # default value
 
 set :application, "Monique_PaymentBoulanger"
 set :repo_url, "git@github.com:Monikku12/Rocket-Elevators-Foundation.git"
-set :ssh_options, { forward_agent: true }
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
